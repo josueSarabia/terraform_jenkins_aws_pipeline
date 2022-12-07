58ffaefc5b381380475cf4f39033c2cb5e995927
josueSarabia <josuedavids@hotmail.com>
2022-12-06 21:33:03 -0500

    fix codedeploy agent installation

--- infra/terraform/modules/compute/main.tf
+++ infra/terraform/modules/compute/main.tf
@@ -40,10 +40,18 @@ resource "aws_instance" "web_server" {
   sudo DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes install docker-ce docker-ce-cli containerd.io docker-compose-plugin
   sudo usermod -aG docker ubuntu
 
-  cd ~
-  wget https://aws-codedeploy-${var.region}.s3.${var.region}.amazonaws.com/latest/install
-  chmod +x ./install
-  sudo ./install auto > /tmp/logfile
+
+  sudo apt-get install ruby-full ruby-webrick wget -y
+  cd /tmp
+  wget https://aws-codedeploy-${var.region}.s3.${var.region}.amazonaws.com/releases/codedeploy-agent_1.3.2-1902_all.deb
+  mkdir codedeploy-agent_1.3.2-1902_ubuntu22
+  dpkg-deb -R codedeploy-agent_1.3.2-1902_all.deb codedeploy-agent_1.3.2-1902_ubuntu22
+  sed 's/Depends:.*/Depends:ruby3.0/' -i ./codedeploy-agent_1.3.2-1902_ubuntu22/DEBIAN/control
+  dpkg-deb -b codedeploy-agent_1.3.2-1902_ubuntu22/
+  sudo dpkg -i codedeploy-agent_1.3.2-1902_ubuntu22.deb
+  sudo systemctl list-units --type=service | grep codedeploy
+  sudo service codedeploy-agent status
+
   EOF
 
 
