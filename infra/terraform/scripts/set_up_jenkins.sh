2f2561b368ebea9ba9c94f21b6fd0e822e768b49
josueSarabia <josuedavids@hotmail.com>
2022-12-10 17:43:22 -0500

    added ecr

--- infra/terraform/scripts/set_up_jenkins.sh
+++ infra/terraform/scripts/set_up_jenkins.sh
@@ -3,8 +3,8 @@ terraform -chdir="../" init
 terraform -chdir="../" plan
 terraform -chdir="../" apply -target="module.s3" -auto-approve
 
-# chmod +x ./upload_volume_s3.sh
-# ./upload_volume_s3.sh
+chmod +x ./upload_volume_s3.sh
+./upload_volume_s3.sh
 
 terraform -chdir="../" apply -target="module.networking" -target="module.s3" -target="module.jenkins" -auto-approve
 
