cf03e53a956eea49617c46d62af554589ed285af
josueSarabia <josuedavids@hotmail.com>
2022-12-20 21:10:33 -0500

    DS-3 [backend] fix codedeploy script start_container

--- infra/terraform/modules/compute/main.tf
+++ infra/terraform/modules/compute/main.tf
@@ -124,7 +124,7 @@ resource "aws_instance" "web_server_staging" {
 
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
   unzip awscliv2.zip
-  sudo ./aws/install
+  sudo ./aws/install 
 
   aws s3 cp s3://${var.bucket_name}/$ARTIFACT_NAME.tar.gz $USER_HOME/$ARTIFACT_NAME.tar.gz
   tar -xf $USER_HOME/$ARTIFACT_NAME.tar.gz -C $USER_HOME/
