d51a09c979ec276e04e37ee91b57fec6d7dd7dc1
josueSarabia <josuedavids@hotmail.com>
2022-12-24 16:43:16 -0500

    DS-4 [frontend] added cadvisor, prom-client. fix docker compose app

--- app/backend/src/main.js
+++ app/backend/src/main.js
@@ -1,9 +1,24 @@
 require('dotenv').config()
 const express = require('express')
+const client = require('prom-client')
 const app = express()
 const cors = require('cors')
 const PORT = 8081
 const getProductsFromDB = require('./db').getProductsFromDB
+
+
+let register = new client.Registry()
+let requestCount = new client.Counter({
+    name: "request_count",
+    help: "number of request to an endpoint"
+})
+register.registerMetric(requestCount)
+
+register.setDefaultLabels({
+    app: 'simple-web-api'
+})
+
+client.collectDefaultMetrics({ register })
 
 let corsOptions = {
     origin: `${process.env.BASE_URL}`
@@ -14,8 +29,14 @@ app.use(cors(corsOptions))
 app.disable("x-powered-by");
 
 app.get('/products', function(req, res) {
+    requestCount.inc(1)
     const productsFromDB = getProductsFromDB()
     res.send({items: productsFromDB});
+});
+
+app.get('/metrics', async function(req, res) {
+    res.setHeader('Content-type', register.contentType)
+    res.end(await register.metrics())
 });
 
 app.listen(PORT, () => {
--- infra/terraform/codedeploy_scripts/start_container.sh
+++ infra/terraform/codedeploy_scripts/start_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # docker run -p 80:8081 -d REPO_URL:COMMIT;
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ECR_REPO_URL
-docker compose up -d
+docker compose -f ./docker-compose/app/FEATURE/docker-compose.yml up -d
