65ec940c05338858f41fcaa9fdf274a0a7ce3efe
josueSarabia <josuedavids@hotmail.com>
2022-12-24 18:36:44 -0500

    DS-4 [backend] fix jenkinsfile

--- infra/terraform/codedeploy_scripts/start_container.sh
+++ infra/terraform/codedeploy_scripts/start_container.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # docker run -p 80:8081 -d REPO_URL:COMMIT;
+service=FEATURE
+
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ECR_REPO_URL
-docker compose -f ./docker-compose/app/FEATURE/docker-compose.yml up -d
+if [[ service = "backend" ]]; then
+    docker network create frontend_default || true
+fi
+sudo ls
+docker compose -f ./docker-compose/app/$service/docker-compose.yml up -d
